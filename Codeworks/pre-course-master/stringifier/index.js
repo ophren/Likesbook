b5a4a46ba24631bf812c8ec9be1ed4ecc1b249e3
Benjamin <ophren_lied@live.fr>
2020-07-26 03:07:16 +0200

    stringifier done

--- Codeworks/pre-course-master/stringifier/index.js
+++ Codeworks/pre-course-master/stringifier/index.js
@@ -1,104 +1,194 @@
 
+/* 
+Stringifier - description
 
-// ECRIRE LA SUPERSTRUCTURE SANS LE CONVERTER, AVEC UN FAUX CONVERTER POUR TESTER LES RECURSIONS
-// ENSUITE DEFINIR LES CONVERSIONS
+-- First, we determine if the input is a simple type (not an object/array). If so, we directly stringify        the result via the converter function (e.g. if the input is a single number, a single function, etc.)
+   *
+   
+-- The "converter" function returns a string for each type of element (e.g. converting arr to string,...)
+     *
+-- The "test" function identifies & replicates the struture of the main input (obj within array, etc.)
+-- The test function is split into two parts, handling respectively arrays [] and objects {}.
+-- The test function returns a result with extra comma. The last for loop removes them and returns
+     the final result -> the variable stringified
 
-// Returns a stringified version of input,
-// behaving in exactly the same way as JSON.stringify()
+My questions : 
+#1 - should return undefined, but print null, why not print undefined?
+#2 - How to 
+
+
+*/
+
+
 function stringifier (input) {
-         
-        var answer = [];
 
-        // If not an array, it's a basic type. No need to call converter via test in that case
-        // This if statement is a shortcut
-        // CAN BE PLACED IN TEST BEFORE THE FOR LOOP
-        if (Array.isArray(input) === false) {
+console.log(input);
+    var res = "";
+    var stringified = "";
 
-            var res = converter (input);
-            return res;
-        }
+    // test with simple variables (excludes arr/obj)
+    if ((input == null || undefined) || typeof input !== "object") {
 
-        else {
-
-            test (input);
-        }
+        return converter(input);
+    }
+ 
+ 
+ 
+    test(input);
 
 
-        //CONVERTER
-        // This function converts any type into a string and pushes it to an array
-        function converter (part) {
+    function converter (part) {
 
              if (typeof part == "number") {
                 var convert_num = part.toString();
                 return part.toString();
             }
 
-            if (typeof part == "function" ) {
+            if (typeof part == "function") { // it returns undefined but it prints null??
 
-                //PROBLEME HERE -> returns two results
-                //answer.push("null");
                 return undefined;
 
             }
 
-            if (typeof part == "string" && typeof part !== "undefined") {
+            if (typeof part == "string") {
                  var test = "\"" + part.toString()+ "\"";
                 return test;
             }
 
              
-               if (part == null && typeof part !== "undefined") {
+             if (part == null && typeof part == "object" ) {
+
+                return "null";
+             }
+
+             /*
+               if ((part == undefined) && typeof part !== "undefined") {
                     var conv_null = "null";
                     return conv_null;
 
+                }*/
+
+
+            
+
+    }
+    
+    
+
+
+
+    function test (input) {
+
+        // If string, number, function, see
+        // See cheat code or code on Sublime Text
+
+
+    // handling arrays
+    if (Array.isArray(input)) {
+
+                res = res + "[";
+
+                for (var i = 0; i < input.length; i++) {
+
+                    if (typeof input[i] == "object" && input[i] !== null) {
+                        test (input[i]);
+                    }
+
+                    else {
+                        var tmp = null;
+
+                            if (converter(input[i]) == undefined) {
+                                res = res + tmp + ",";
+                            }
+
+                            else {
+
+                            res = res + converter(input[i]) + ",";}
+                    }
+
                 }
 
+                res = res + "]" + ",";
+    }
 
-                 
-        }    
 
-    //can arrays be handled like objects??
-    //test -> rename "stringify array"
-                  
-        //CALLER if array
-        //Identifies the dimensions of the array and calls converter with each value of any array
-        function test (input) {
-            for (var i = 0; i < input.length; i++) {
+    //handling objects
+    if (typeof input == "object" && Array.isArray(input) == false) {
 
-                  if (Array.isArray(input[i])) {
-    //STRINGIFIER REPLACED BY TEST
-                    test (input[i]);
+        res = res + "{";
 
-                  }
-                  
-                  else {
-                 //insert recursion here
+              for (var key in input) {
 
-                    var res = converter(input[i]);
-                    
-                    answer.push(res);
+                    //ignoring object prototype
+                    if (input.hasOwnProperty(key) == false) {
 
-            // THIS IS NO RECURSION!!        
-                    
+                        continue;
+                }
+
+                    //4 cases
+
+                    if (typeof key == "object") {
+
+                      test (key);
+                    }
+
+                    //ignoring function
+                    if (typeof key !== "object" && typeof input[key] !== "function" && typeof input[key] !== "undefined") {
+
+                      res = res + "\""+key+"\"" + ":";
+                    }
+
+
+                    if (typeof input[key] == "object") {
+
+                        if (input[key] == null) {
+
+                            res = res + converter(input[key]) + ",";
+                        }
+
+                        else {
+
+                        test (input[key]);
+                        }
+                    }
+
+
+                    if (typeof input[key] !== "object" && typeof input[key] !== "function" && typeof input[key] !== "undefined") {
+
+                        res = res + converter(input[key]) + ",";
+                    }
+
               }
 
-            }
-        
+            res = res + "},";
 
-          
-        } 
+    }
 
 
-          var stringified = answer.join();
-         
-          //if statement -> if one dimension, if two dimensions, etc.
-          return "[" + stringified + "]";
+    }
 
-        
-// -------- TESTS -------
 
+    // LAST STEP -> STRINGIFY RESULT (removing useless commas) 
+
+      for (var i=0; i<res.length; i++) {
+
+                if ((i == (res.length-1) && res.charAt(i) == ",") 
+                    || res.charAt(i) == "," && (res.charAt(i+1) == "}" || res.charAt(i+1) =="]")) {
+                          continue;
+                      }
+
+                     else {
+
+                       stringified = stringified + res.charAt(i);}
+      }
+
+  
+     return stringified;
 
 }
+
+
+
 // Allow tests to run on the server (leave at the bottom)
 if (typeof window === 'undefined') {
   module.exports = stringifier;
